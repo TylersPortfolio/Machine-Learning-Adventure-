9e8d7d8b190ead17976b5ddab023d5a8b7eb0aaa
TylersPortfolio <46131742+TylersPortfolio@users.noreply.github.com>
2019-01-03 23:53:01 +0300

    Add files via upload

---
+++ Machine-Learning-1.py
@@ -0,0 +1,12 @@
+from sklearn import tree
+
+# 1 = medium shirt
+# 2 = Large shirt
+# The first number's in features_of_shirts = inches
+
+features_of_shirts = [38, 2], [44, 2], [38, 1], [44, 1]
+shirts = [2, 2, 1, 1]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_of_shirts, shirts)
+print(clf.predict([[44, 2]]))
