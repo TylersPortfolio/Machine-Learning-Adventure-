3ac103def88eef3a2cd919819ad7a91b1998598e
TylersPortfolio <46131742+TylersPortfolio@users.noreply.github.com>
2019-01-06 01:31:33 +0300

    Add files via upload

---
+++ Ways-To-Visualize-Data.py
@@ -0,0 +1,46 @@
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+
+
+# Load Iris Dataset
+
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ["sepal-length", "sepal-width", "petal-length", "petal-width", "class"]
+dataset = pandas.read_csv(url, names=names)
+
+# Shape of the Data
+#print(dataset.shape)
+
+#------------------------------------------#
+
+# Head of the Data
+#print(dataset.head(40))
+
+#------------------------------------------#
+
+# Discriptions of Datase
+#print(dataset.describe())
+
+#------------------------------------------#
+
+# class distribution of Data
+#print(dataset.groupby("class").size())
+
+#------------------------------------------#
+
+# Box and Whisker Plots of Iris Data
+#dataset.plot(kind = "box", subplots = True, layout = (2, 2), sharex = False, sharey = False,)
+#plt.show()
+
+#------------------------------------------#
+
+# Histogram of Data
+#dataset.hist()
+#plt.show()
+
+#------------------------------------------#
+
+# Scatter Plot Matrix
+#scatter_matrix(dataset)
+#plt.show()
